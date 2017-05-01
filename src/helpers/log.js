eec7ebd0e316f7be6e5c81b30bf59cf9c5c3029a
Nedrae <nedraenintyel@gmail.com>
2017-05-01 15:23:01 -0500

    Properly stub out console helper methods

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -3,10 +3,14 @@ const env = process.env.NODE_ENV;
 const log = {};
 
 for (const method in console) {
+  let func = () => {};
+
   if (env === 'development') {
     // eslint-disable-next-line
-    log[method] = console[method];
+    func = console[method];
   }
+
+  log[method] = func;
 }
 
 export default log;
