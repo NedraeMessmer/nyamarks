2cc46c9b605be5b62ffd806dc23b0e312af98877
Nedrae <nedraenintyel@gmail.com>
2017-05-10 00:26:50 -0500

    Pull new link form into its own component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,29 +2,7 @@
   <div id="main">
     <h1>{{welcome}}</h1>
 
-    <form class="new-link" name="newLink" @submit.prevent="addLink">
-      <h2>
-        <a @click.prevent="toggleNewLink">
-          Add new link
-          <span
-            class="fa fa-fw fa-chevron-down fa-animated"
-            :class="{'fa-flip-vertical': showNewLink}"></span>
-        </a>
-      </h2>
-
-      <transition name="fold">
-        <div class="new-link-form" v-show="showNewLink">
-          <p><input placeholder="Name" v-model.trim="newLink.name"></p>
-          <p><input placeholder="URL (required)" v-model.trim="newLink.url" required></p>
-          <p><input placeholder="Tags (space-delimited)" v-model.trim="newLink.tags"></p>
-          <p>
-            <textarea placeholder="Description" v-model.trim="newLink.description"></textarea>
-          </p>
-
-          <button type="submit">Add link</button>
-        </div>
-      </transition>
-    </form>
+    <new-link toggle></new-link>
 
     <link-list></link-list>
 
@@ -44,54 +22,24 @@
 </template>
 
 <script>
+import NewLink from '@/components/NewLink';
 import LinkList from '@/components/LinkList';
 import TagList from '@/components/TagList';
-
-const newLink = {
-  name: '',
-  url: '',
-  tags: '',
-  description: '',
-};
 
 export default {
   name: 'Main',
   components: {
+    NewLink,
     LinkList,
     TagList,
   },
   data() {
     return {
       welcome: 'Welcome to nyamarks',
-      newLink: {...newLink},
-      fileLoad: null,
       debugTags: false,
-      showNewLink: false,
     }
   },
   methods: {
-    toggleNewLink() {
-      return this.showNewLink = !this.showNewLink;
-    },
-
-    addLink() {
-      const link = {...this.newLink};
-      const form = document.forms.newLink;
-
-      // Normalize tags
-      if (link.tags === '') {
-        link.tags = [];
-      } else {
-        link.tags = link.tags.split(' ').sort();
-      }
-
-      // Reset form
-      form.reset();
-      this.newLink = {...newLink};
-
-      this.$store.dispatch('addLink', {link});
-    },
-
     debugToggleTags() {
       return this.debugTags = !this.debugTags;
     },
@@ -106,33 +54,5 @@ export default {
 
 h1, h2 {
   font-weight: normal;
-}
-
-.new-link {
-  input, textarea {
-    width: 25vw;
-  }
-
-  .linkArrow {
-    display: inline-block;
-    transition: transform 0.25s;
-
-    &.toggled {
-      transform: rotateX(-180deg);
-    }
-  }
-}
-
-// Transitions
-.fold-enter-active,
-.fold-leave-active {
-  transform-origin: top center;
-  transition: opacity 0.25s, transform 0.25s;
-}
-
-.fold-enter,
-.fold-leave-to {
-  opacity: 0;
-  transform: scaleY(0);
 }
 </style>
---
+++ src/components/NewLink.vue
@@ -0,0 +1,110 @@
+<template>
+  <form class="new-link" name="newLink" @submit.prevent="addLink">
+    <h2>
+      <a @click.prevent="toggleForm" v-if="toggle">
+        Add new link
+        <span
+          class="fa fa-fw fa-chevron-down fa-animated"
+          :class="{'fa-flip-vertical': showForm}"></span>
+      </a>
+      <span v-if="!toggle">Add new link</span>
+    </h2>
+
+    <transition name="fold">
+      <div v-show="!toggle || showForm">
+        <p><input placeholder="Name" v-model.trim="newLink.name"></p>
+        <p><input placeholder="URL (required)" v-model.trim="newLink.url" required></p>
+        <p><input placeholder="Tags (space-delimited)" v-model.trim="newLink.tags"></p>
+        <p>
+          <textarea placeholder="Description" v-model.trim="newLink.description"></textarea>
+        </p>
+
+        <button type="submit">Add link</button>
+      </div>
+    </transition>
+  </form>
+</template>
+
+<script>
+const newLink = {
+  name: '',
+  url: '',
+  tags: '',
+  description: '',
+};
+
+export default {
+  name: 'AddLink',
+  props: {
+    toggle: {type: Boolean, required: false, default: false},
+  },
+  data() {
+    return {
+      newLink: {...newLink},
+      showForm: !this.toggle,
+    }
+  },
+  methods: {
+    toggleForm() {
+      return this.showForm = !this.showForm;
+    },
+    addLink() {
+      const link = {...this.newLink};
+      const form = document.forms.newLink;
+
+      // Normalize tags
+      if (link.tags === '') {
+        link.tags = [];
+      } else {
+        link.tags = link.tags.split(' ').sort();
+      }
+
+      // Reset form
+      form.reset();
+      this.newLink = {...newLink};
+
+      this.$store.dispatch('addLink', {link});
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.new-link {
+  input, textarea {
+    width: 25vw;
+  }
+
+  button {
+    background-color: rgb(64, 0, 192);
+    border: none;
+    border-radius: 2px;
+    box-shadow: inset 0 -3px rgba(0, 0, 0, 0.5);
+    color: white;
+    cursor: pointer;
+    font-size: 1em;
+    font-weight: bold;
+    margin: 2px;
+    padding: 5px 8px 8px;
+    text-decoration: none;
+    text-shadow: 0 -1px rgba(0, 0, 0, 0.5);
+    transition: background-color 0.2s ease-in;
+
+    &:hover {
+      background-color: mix(rgb(64, 0, 192), white, 50%);
+    }
+  }
+}
+
+.fold-enter-active,
+.fold-leave-active {
+  transform-origin: top center;
+  transition: opacity 0.25s ease-in, transform 0.25s ease-in;
+}
+
+.fold-enter,
+.fold-leave-to {
+  opacity: 0;
+  transform: scaleY(0);
+}
+</style>
