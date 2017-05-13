df05d033ed8d492826fa0504447aa03b5f24d72e
Nedrae <nedraenintyel@gmail.com>
2017-05-13 12:20:14 -0500

    Fix history mode for gh-pages...?

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import Main from '@/components/Main'
 Vue.use(Router)
 
 export default new Router({
+  base: process.env.NODE_ENV === 'production' ? '/nyamarks/' : '/',
+  mode: 'history',
   routes: [
     {
       path: '/',
